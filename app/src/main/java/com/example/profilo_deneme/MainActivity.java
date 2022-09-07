c544bda61f5bda2400dae00ade930215ae320767
kerem <keremozbl>
2022-09-07 11:18:50 +0300

    Button_Click_Test

---
+++ app/src/main/java/com/example/profilo_deneme/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.profilo_deneme;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    Button add,update ;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        add     =findViewById(R.id.button);          // butonları javaya tanıttık
+        update  =findViewById(R.id.button2);        // butonları javaya tanıttık
+
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                add.setText("BASILDI");
+            }
+        });
+        update.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                add.setText("İPTAL EDİLDİ");
+            }
+        });
+
+
+
+
+
+    }
+}
